e6d6c28a0a760e18f66496009f17469566d70c5d
umitotuki <umitukiuk@gmail.com>
2024-06-05 05:11:03 +0000

    activatrage install

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
    registrations: "public/registrations",
   sessions: 'public/sessions'
  }
-   devise_for :admins, skip: [:registrations, :passwords], controllers: {
-     sessions: "admin/sessions"
-   }
+  # devise_for :admins, skip: [:registrations, :passwords], controllers: {
+  #   sessions: "admin/sessions"
+  # }
  
   namespace :public do
     get 'users/index'
