9f7c2dce5cd59f4b6cb4f16394166f817ea1b394
umitotuki <umitukiuk@gmail.com>
2024-06-05 00:51:58 +0000

    controller作成

---
+++ app/controllers/public/users_controller.rb
@@ -0,0 +1,16 @@
+class Public::UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def confirm
+  end
+
+  def mypage
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,38 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :admins
+  namespace :public do
+    get 'users/index'
+    get 'users/show'
+    get 'users/edit'
+    get 'users/confirm'
+    get 'users/mypage'
+  end
+   devise_for :users
+   
+  namespace :public do
+    get 'chats/show'
+  end
+  namespace :public do
+    get 'posts/new'
+    get 'posts/index'
+    get 'posts/show'
+    get 'posts/edit'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'users/index'
+    get 'users/show'
+    get 'users/edit'
+  end
+  namespace :admin do
+    get 'posts/index'
+    get 'posts/show'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
