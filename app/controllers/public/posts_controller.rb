fa900256c49e7f55b5fc9f3cfb8c576387fc3f78
umitotuki <umitukiuk@gmail.com>
2024-06-06 11:55:27 +0000

    post途中まで

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -2,12 +2,27 @@ class Public::PostsController < ApplicationController
   before_action :authenticate_user!
   
   def new
+    @post = Post.new
   end
-
+  
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if  @post.save
+    flash[:notice] = "投稿に成功しました。"
+    redirect_to post_path(@post)
+    else
+    flash.now[:alert] = "投稿に失敗しました"
+      render 'new'
+    end
+  end
+  
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
