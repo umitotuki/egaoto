7b9a3215e333fb5fe5142df229a4a889b28ebb39
umitotuki <umitukiuk@gmail.com>
2024-06-11 01:50:07 +0000

    ユーザー関連修正

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -25,10 +25,24 @@ class Public::UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:notice] = "You have updated user successfully."
+      flash[:notice] = "更新しました。"
       redirect_to user_path(@user)
     else
+      flash.now[:alert] = "更新できませんでした。"
       render :edit
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :birth, :location, :introduction, :is_active, :image)
+  end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user)
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,13 @@ class User < ApplicationRecord
          
   has_many :posts, dependent: :destroy
   
-  has_one_attached :profile_image
+  has_one_attached :image
   
-  def get_profile_image
-    unless profile_image.attached?
+  def get_image
+    unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize_to_limit: [100, 100]).processed
+    image.variant(resize_to_limit: [100, 100]).processed
   end
 end
