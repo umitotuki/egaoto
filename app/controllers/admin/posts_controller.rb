7904a1bfb94407b1121af75f92081f65ea0b75c3
umitotuki <umitukiuk@gmail.com>
2024-06-18 23:34:35 +0000

    admin show

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,10 +1,17 @@
 class Admin::PostsController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def index
-   
+
   end
 
   def show
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to admin_top_path
   end
 end
