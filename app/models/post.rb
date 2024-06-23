f4a1f1f789ccb2dae7e6f4a97bd6e9037fb308d2
umitotuki <umitukiuk@gmail.com>
2024-06-23 02:40:57 +0000

    favorite途中

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,11 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :genre
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+   favorites.exists?(user_id: user.id)
+  end
 
   def get_image(width, height)
     unless image.attached?
