4399dc3f32226d06be9de5a98c934411072ea5f3
umitotuki <umitukiuk@gmail.com>
2024-06-24 02:48:43 +0000

    フォロー機能途中

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+
+  # フォローする
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # フォローされる
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # フォローしているユーザーの取得
+  has_many :followings, through: :active_relationships, source: :followed
+  # フォローされているユーザーの取得(フォロワー)
+  has_many :followers, through: :passive_relationships, source: :follower
   has_one_attached :image
-  
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -16,7 +25,7 @@ class User < ApplicationRecord
     end
     image.variant(resize_to_limit: [100, 100]).processed
   end
-  
+
   GUEST_USER_EMAIL = "guest@example.com"
   GUEST_USER_BIRTH = "xxxx年xx月xx日"
 
@@ -26,11 +35,23 @@ class User < ApplicationRecord
       user.name = "guestuser"
     end
   end
-  
+
   validates :name, presence: true
-  
+
   def self.looks(search)
       @records = User.where("name LIKE?","%#{search}%")
+  end
+
+  def follow(user)
+    active_relationships.create(followed_id: user.id)
+  end
+
+  def unfollow(user)
+    active_relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
   end
 
 end
